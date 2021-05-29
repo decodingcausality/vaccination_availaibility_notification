813a74135cb2d4563ba876c904df3820d5aba96f
decodingcausality <81964323+decodingcausality@users.noreply.github.com>
2021-05-29 11:37:31 +0530

    updated version with multiprocessing

--- get_vaccine_info.py
+++ get_vaccine_info.py
@@ -16,26 +16,27 @@ import send_sms
 import pprint
 import sys
 
-
-#Generate Execution Date
+# Generate Execution Date
 date = datetime.date.today().strftime("%d-%m-%Y")
 
+
 def get_user_inputs(date):
-    try :
+    try:
         # input("Please enter mobile number on which you would like to get notified")
 
-        print(f"***********************************Getting Vaccination Info********************************************")
+        print(
+            f"***********************************Getting Vaccination Info********************************************")
 
         option = input("""Would you Like to Search by entering PIN CODE or selecting from available STATES ? 
         IF PINCODE then enter 1 or for STATE enter 2 : """)
-        if option == str(1) :
+        if option == str(1):
 
             # Get user inputs for PIN code based search
             pin_code = input("Please enter PIN CODE ")
 
             available_capacity, min_age_limit, name, address = get_info_for_pin_code(pin_code, date)
 
-            return option,pin_code,available_capacity,min_age_limit,name,address
+            return option, pin_code, available_capacity, min_age_limit, name, address
 
         elif option == str(2):
             #   logic for Searching based on State
@@ -44,28 +45,27 @@ def get_user_inputs(date):
             response_all_states = response_all_states.json()
             total_states = len(response_all_states['states'])
             print(f"Total States : {total_states}")
-            for i in range(total_states) :
+            for i in range(total_states):
                 state_id = response_all_states['states'][i]['state_id']
                 state_name = response_all_states['states'][i]['state_name']
                 print(f"State Name : {state_name}, State ID : {state_id} ")
 
-
-            #Get Input for State ID from User
+            # Get Input for State ID from User
             state_id = input("Enter State ID : ")
-            #get response for states
+            # get response for states
 
             # # Request URL For State ID Search
             request_url_state = f"{constants.FIND_BY_STATE_ENDPOINT}{state_id}"
 
-            response_state = requests.get(url = request_url_state, headers=constants.headers)
+            response_state = requests.get(url=request_url_state, headers=constants.headers)
             print(f"Response received for state : {response_state}")
             status_code = response_state.status_code
             print(f"status_code {status_code}")
-            if status_code ==  200 :
+            if status_code == 200:
                 response_state = response_state.json()
                 total_districts = len(response_state['districts'])
                 print(f"Total Number of Vaccination center for State Code : {config.state_id} are {total_districts}")
-                for i in range(total_districts) :
+                for i in range(total_districts):
                     district_id = response_state['districts'][i]['district_id']
                     district_name = response_state['districts'][i]['district_name']
                     print(f"district ID : {district_id} : District Name : {district_name}")
@@ -75,17 +75,18 @@ def get_user_inputs(date):
             # Generate request url for district code
             request_url_district = f"{constants.FIND_BY_DISTRICT_ENDPOINT}{district_id}&date={date}"
             pin_code = ""
-            return option,pin_code,request_url_district
-    except Exception as e :
+            return option, pin_code, request_url_district
+    except Exception as e:
         print(e)
 
-def get_info_for_pin_code(pin_code,date):
-    try :
+
+def get_info_for_pin_code(pin_code, date):
+    try:
 
         request_url_pin = f"{constants.FIND_BY_PIN_ENDPOINT}{pin_code}&date={date}"
 
         # Get request for slot availability based on pin
-        response_pin = requests.get(url = request_url_pin, headers=constants.headers)
+        response_pin = requests.get(url=request_url_pin, headers=constants.headers)
         status_code = response_pin.status_code
 
         response = response_pin.json()
@@ -128,34 +129,36 @@ def get_info_for_pin_code(pin_code,date):
             print(f"Available capacity : {available_capacity}")
             print(f"************{type(min_age_limit)}")
 
-            if min_age_limit == 18 :
+            if min_age_limit == 18:
                 print(f"Shots available for 18 + range of age group : {available_capacity}")
-                print(f"###############################@@@@@@Shots available for 18 + range of age group : {available_capacity}")
+                print(
+                    f"###############################@@@@@@Shots available for 18 + range of age group : {available_capacity}")
                 # Set Availaibility Flag to true
                 available_flag = True
                 # set age group flag
                 age_group = "18+"
-                return available_flag, age_group,option,pin_code,available_capacity,min_age_limit,name,address
+                return available_flag, age_group, option, pin_code, available_capacity, min_age_limit, name, address
 
             elif min_age_limit == 45:
                 print(f"Shots available for 45 +  : {available_capacity}")
                 available_flag = True
                 # set age group flag
                 age_group = "45+"
-                return available_flag, age_group,option,pin_code,available_capacity,min_age_limit,name,address
+                return available_flag, age_group, option, pin_code, available_capacity, min_age_limit, name, address
         option = ""
-        return option,available_capacity, min_age_limit ,name, address,available_flag, age_group
-    except Exception as e :
+        return option, available_capacity, min_age_limit, name, address, available_flag, age_group
+    except Exception as e:
         print(e)
 
 
 def set_flags(available_capacity, min_age_limit):
-    try :
-        #notification flag for 18 and above :
+    try:
+        # notification flag for 18 and above :
         # Default Flags
         available_flag = False
         if min_age_limit < 45 and available_capacity > 0:
-            print(f"###############################@@@@@@Shots available for 18 + range of age group : {available_capacity}")
+            print(
+                f"###############################@@@@@@Shots available for 18 + range of age group : {available_capacity}")
             # Set Availaibility Flag to true
             available_flag = True
             # set age group flag
@@ -170,15 +173,15 @@ def set_flags(available_capacity, min_age_limit):
         else:
             print("error fetching data")
 
-    except Exception as e :
+    except Exception as e:
         print(e)
 
 
 # Driver Code
 if __name__ == '__main__':
-    try :
+    try:
         option, pin_code, request_url_district = get_user_inputs(date)
-        while True :
+        while True:
 
             # get response for district
             response_district = requests.get(url=request_url_district, headers=constants.headers)
@@ -235,5 +238,5 @@ if __name__ == '__main__':
                     time.sleep(5)
             print(f"***********************************************{min_age_limit}")
 
-    except Exception as e :
-            print(e)
+    except Exception as e:
+        print(e)
